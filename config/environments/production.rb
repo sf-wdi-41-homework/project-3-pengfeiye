ac6a3e48e5dbd8535a861c108c7a93e4fe2d57fb
pengfei <pengfeiye1994@gmail.com>
2017-12-06 15:13:10 -0800

    heroku push

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,9 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  /config/environments/production.rb
+
+  config.middleware.use ChatActionCable
+  config.web_socket_server_url = "https://ancient-hamlet-73557.herokuapp.com/"
 end
