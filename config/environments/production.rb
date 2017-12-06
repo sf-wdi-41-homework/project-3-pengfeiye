d85de6245b1804dfc31329a233dd41e945f831d5
pengfei <pengfeiye1994@gmail.com>
2017-12-06 15:45:11 -0800

    heroku push

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  config.middleware.use ChatActionCable
+config.web_socket_server_url = "wss://your-heroku-app.herokuapp.com/"
 
   # Code is not reloaded between requests.
   config.cache_classes = true
