52e973edcf2bc5e76053d40768969bee64f75b2d
pengfei <pengfeiye1994@gmail.com>
2017-12-07 12:11:34 -0800

    text box scroll bottom fixed

--- app/assets/javascripts/chatboxes.js
+++ app/assets/javascripts/chatboxes.js
@@ -1,13 +1,19 @@
 $(document).on('turbolinks:load',function(){
+  updateScroll()
+
   $('#msgInput').on('keypress', function(e){
     if(e.keyCode === 13){
       e.preventDefault()
       $('#msgForm').submit()
       $('#msgInput').val('')
+      updateScroll()
       }
     });
 
-
+    function updateScroll(){
+      $(".msg").animate({ scrollTop: $(this).height() }, "slow");
+      return false;
+    }
 
     $(".signup_link").click(function(e) {
       e.preventDefault()
