b6bde4f25d9d160a986ff97ea9a33918c59b77e8
pengfei <pengfeiye1994@gmail.com>
2017-12-11 09:46:29 -0800

    scroll bar fix to only scroll on show page

--- app/assets/javascripts/chatboxes.js
+++ app/assets/javascripts/chatboxes.js
@@ -6,22 +6,7 @@ $(document).on('turbolinks:load',function(){
    setTimeout(function(){
        App.cable.connect();
    }, 500);
-  updateScroll()
-  console.log()
 
-  $('#msgInput').on('keypress', function(e){
-    if(e.keyCode === 13){
-      e.preventDefault()
-      $('#msgForm').submit()
-      $('#msgInput').val('')
-      updateScroll()
-      }
-    });
-
-
-  function updateScroll(){
-    $(".msg").animate({ scrollTop: $(".msg")[0].scrollHeight }, "fast");
-  }
 
   $(".signup_link").click(function(e) {
     e.preventDefault()
